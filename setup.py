8a2f51bba41975f3627cacf37ed1adc855c660cb
Ryoo Kyungphil <97349773+RyooKP@users.noreply.github.com>
2022-06-15 12:56:33 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from seyuptools import setup
+
+setup(
+  name='bert_setup',
+  version='0.0.1',
+  description='bert setup for project',
+  url='https://github.com/RyooKP/bert_setup.git',
+  author='kp.ryoo',
+  author_email='kp.ryoo@yonsei.ac.kr',
+  license=' ',
+  pakages=['bert_setup'],
+  zip_safe=False,
+  install_requies=['']
+)
